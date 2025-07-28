7fa60dd8328331594972da6c38b33b5fda2ea4f7
gomezrjo <gomezrjo@gmail.com>
2025-07-28 11:24:04 -0400

    Update validation script

--- utilities/check-pre-reqs.sh
+++ utilities/check-pre-reqs.sh
@@ -3,14 +3,14 @@
 if [ ! command -v oc &> /dev/null ]; then echo "oc could not be found"; exit 1; fi;
 if [ ! command -v awk &> /dev/null ]; then echo "awk could not be found"; exit 1; fi;
 echo "Checking required operators to use pipelines..."
-if [ ! -z $(oc get csv | awk '$1 ~ "openshift-pipelines-operator-rh" {print "True"}') ]
+if [ ! -z $(oc get csv --no-headers --ignore-not-found | awk '$1 ~ "openshift-pipelines-operator-rh" {print "True"}') ]
 then
     echo "RedHat OpenShift Pipelines Operator Pass"
 else
     echo "RedHat OpenShift Pipelines Operator Fail"
 fi
 #
-if [ ! -z $(oc get csv | awk '$1 ~ "ibm-tz-deployer" {print "True"}') ]
+if [ ! -z $(oc get csv --no-headers --ignore-not-found | awk '$1 ~ "ibm-tz-deployer" {print "True"}') ]
 then
     echo "IBM TZ Deployer Operator Pass"
 else
@@ -19,7 +19,7 @@ fi
 #
 echo
 echo "Checking storage classes..."
-if [ ! -z $(oc get sc --no-headers | wc -l | awk '{if ($1+0 > 0) print "True"}') ]
+if [ ! -z $(oc get sc --no-headers --ignore-not-found  | wc -l | awk '{if ($1+0 > 0) print "True"}') ]
 then
     echo "Storage Classes Pass"
 else
